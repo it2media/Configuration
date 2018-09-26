445248f20672a9e7e36578cd60bcc6dfa12e1e2b
Markus Malz <markus.malz@it2media.de>
2018-09-26 11:22:42 +0200

    Extend OptionsServiceCollectionExtensions

--- src/IT2media.Extensions.Configuration/OptionsBinder.cs
+++ src/IT2media.Extensions.Configuration/OptionsBinder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 namespace IT2media.Extensions.Configuration
@@ -32,6 +33,31 @@ namespace IT2media.Extensions.Configuration
             }
         }
 
+        public void ConfigureOptions(IServiceCollection serviceCollection)
+        {
+            if (serviceCollection == null) throw new ArgumentNullException(nameof(serviceCollection));
+
+            var optionModels = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(t => t.GetTypes())
+                .Where(t => t.Namespace != null && t.IsClass && t.Namespace.EndsWith(IdentifierForOptionModels) && t.Name.EndsWith(IdentifierForOptionModels) && !t.Name.Equals(IdentifierForOptionModels));
+
+            foreach (var optionModel in optionModels)
+            {
+                var actionMethodInfo = GetType().GetMethod(nameof(BindAction));
+                var genericActionMethodInfo = actionMethodInfo?.MakeGenericMethod(optionModel);
+                var actionToPass = genericActionMethodInfo?.Invoke(this, null);
+
+                var methodInfo = typeof(OptionsServiceCollectionExtensions).GetMethods().First(mi => mi.Name == "Configure");
+                var generic = methodInfo?.MakeGenericMethod(optionModel);
+                generic?.Invoke(serviceCollection, new[] {serviceCollection, actionToPass});
+            }
+        }
+
+        public Action<T> BindAction<T>() where T : class, new()
+        {
+            return GetSectionAndBind;
+        }
+
         public void RegisterOption<T>(Action<Type, object> registerInstance) where T : class, new()
         {
             var instance = CreateConfiguredInstance<T>();
@@ -40,11 +66,16 @@ namespace IT2media.Extensions.Configuration
 
         private IOptions<T> CreateConfiguredInstance<T>() where T: class, new()
         {
-            var optionsModel = (T) Activator.CreateInstance(typeof(T));
+            var optionsModel = (T)Activator.CreateInstance(typeof(T));
+            GetSectionAndBind(optionsModel);
+            return Options.Create(optionsModel);
+        }
+
+        private void GetSectionAndBind<T>(T optionsModel) where T : class, new()
+        {
             var key = $"{IdentifierForOptionModels}:{typeof(T).Name}";
             var configurationSection = _configuration.GetSection(key);
             configurationSection.Bind(optionsModel);
-            return Options.Create(optionsModel);
         }
     }
 }
